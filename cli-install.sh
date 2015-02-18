4c51efbb4b6207331e3c724b2ad5befe53825585
parotikov <parotikov@users.noreply.github.com>
2015-02-19 00:27:26 +0300

    Update cli-install.sh

--- cli-install.sh
+++ cli-install.sh
@@ -1,11 +1,32 @@
-#!/bin/bash
+#!/bin/bash 
+clear   
+bold=`tput bold`
+normal=`tput sgr0`
+
 red='\033[1;31m'
-green='\033[1;32m'
-NC='\033[0m' # No Color
+green='\033[1;32m'        
+blue='\033[1;34m'        
+NC='\033[0m' # No Color   
+
+cat << EOF
++--------------------------------------------+
+|                                            |
+|       Wordpress CLI Installer 1.01         |
+|                                            |
+|  https://github.com/parotikov/wp-cli-bash  |
+|                                            |
+|    Step-by-step semi-automatic install     |
+|    latest WP with some useful plugins      |
+|                                            |
+|         wp-cli must be installed           |
+|  see http://wp-cli.org/ for instructions   |
+|                                            |
++--------------------------------------------+
+EOF
 
 ##if test -f "index.php"; then echo "Dir not empty"; exit 1; fi
 
-printf "\nSelect locale [1 - ru, 2 - en]: "
+printf "\n${bold}Select locale${normal} [1 - ru, 2 - en]: "
 
 read language
 
@@ -19,36 +40,65 @@ case $language in
 	*)
 	printf "\n${red}Locale not selected.${NC} Bye \n\n"; exit 1;
 esac
-printf "\nDownloading...\n" 
+printf "\n${bold}Downloading${normal}...\n" 
 
 wp core download --locale="$locale" || { 
 ##printf "\nDo you want to continue?\n"; 
 
-  read -r -p "Do you want to continue? [y/N] " response
+  read -r -p "${bold}Do you want to continue? ${normal}[y/N] " response
   response=${response,,}    # tolower
   if ! [[ $response =~ ^(yes|y)$ ]]  
   then 
+    printf "\n${bold}Aborting...${normal} Bye\n\n"
     exit 1;
   fi
 
 }
- 
-printf  "\nLets create wp_config.php"
-printf "\nEnter dbname: "
-read dbname
 
-printf "\nEnter dbuser: "
-read dbuser
+if test -f "wp-config.php"; 
+then 
+  read -r -p "${bold}Seems that wp-config.php already created. What we gonna do?${normal} [${bold}O${normal}verwrite/${bold}U${normal}se/${bold}A${normal}bort] " response
+    response=${response,,}    # tolower
+    if [[ $response =~ ^(overwrite|o)$ ]]  
+    then 
+      printf "\n${bold}Backuping existed config to wp-config.php.bak ${normal}\n\n"
+      mv wp-config.php wp-config.php.bak
+    fi
+    if [[ $response =~ ^(abort|a)$ ]]  
+    then 
+      printf "\n${bold}Aborting...${normal} Bye\n\n"
+      exit 1;        
+    fi 
+fi
 
-printf "\nEnter dbpass: "
-read dbpass
-printf  "\nCreating wp-config.php...\n"
 
-wp core config --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" || {
-printf  "\n${red}Error${NC}: Seems that db connections cannot estabished. Please check db properties and try again. Bye\n\n"; exit 1;
-}
 
-printf  "\nNow enter site parameters: "
+if ! [[ $response =~ ^(use|u|'')$ ]]   
+then  
+  printf  "\n${bold}Lets create wp_config.php${normal}"
+  printf "\nEnter dbname: "
+  read dbname
+
+  printf "\nEnter dbuser: "
+  read dbuser
+
+  printf "\nEnter dbpass: "
+  read dbpass
+  printf  "\n${bold}Creating wp-config.php...${normal}\n"
+
+  wp core config --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" || {
+  printf  "\n${red}Error${NC}: Seems that db connections cannot estabished. Please check db properties and try again. Bye\n\n"; exit 1;
+  }
+fi
+
+if [[ $response =~ ^(use|u|'')$ ]]   
+then  
+  printf  "\n${bold}Using existing wp_config.php with follow parameters${normal}\n" 
+  grep DB_NAME wp-config.php
+  grep DB_USER wp-config.php
+  grep DB_PASS wp-config.php
+fi  
+printf  "\n${bold}Now enter site parameters${normal}"
 
 printf "\nEnter hostname without http and slashes: "
 read domain
@@ -65,7 +115,7 @@ read pass
 printf "\nEnter admin email: "
 read adm_email
 
-printf  "\nInstalling, please wait... "
+printf  "\n${bold}Installing, please wait...${normal} "
 printf "\n"
 if ! $(wp core is-installed); then
     wp core install --url=$domain --title="$title" --admin_user="$login" --admin_password="$pass" --admin_email="$adm_email"
@@ -91,7 +141,7 @@ plugins_array=(
 
 for i in "${plugins_array[@]}"
 do
-  read -r -p "Do you want to install $i? [y/N] " response
+  read -r -p "${bold}Do you want to install $i?${normal} [y/N] " response
   response=${response,,}    # tolower
   if [[ $response =~ ^(yes|y)$ ]]  
   then 
@@ -99,3 +149,12 @@ do
 	  wp plugin activate $i
   fi
 done
+
+printf "\n${green}Done!${NC}\n"
+read -r -p "${bold}Do you wanna delete this installation script?${normal} [y/N] " response
+  response=${response,,}    # tolower 
+  printf "\n${bold}Bye.${normal}\n\n"
+  if [[ $response =~ ^(yes|y)$ ]]  
+  then 
+    rm cli-install.sh 
+  fi
